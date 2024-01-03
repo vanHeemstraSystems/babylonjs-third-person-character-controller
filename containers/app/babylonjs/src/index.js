3ce78c484412fd51b5ab6ec5edcbd74cc4da87f7
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-01-03 13:36:48 +0100

    Update index.js

--- containers/app/babylonjs/src/index.js
+++ containers/app/babylonjs/src/index.js
@@ -1,1 +1,15 @@
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  SceneLoader,
+  CreateGround,
+  ExecuteCodeAction,
+  ActionManager
+} from '@babylonjs/core';
+
+import '@babylonjs/loaders';
+
 
