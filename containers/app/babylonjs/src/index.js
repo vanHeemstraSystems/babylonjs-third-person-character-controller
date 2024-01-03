bdbc8cf1ad161f24e9bfd3707b059dcaa626a9e2
Willem van Heemstra <wvanheemstra@icloud.com>
2024-01-03 12:44:15 +0000

    Add code

--- containers/app/babylonjs/src/index.js
+++ containers/app/babylonjs/src/index.js
@@ -12,4 +12,18 @@ import {
 
 import '@babylonjs/loaders';
 
+const canvas = document.getElementById('renderCanvas');
+const engine = new Engine(canvas, true);
+const scene = new Scene(engine);
 
+new HemisphericLight('hemiLight', new Vector3(0, 1, 0));
+
+// Camera
+const camera = new ArchRotatecamera(
+  'arcRotatecamera',
+  0,
+  1,
+  10,
+  new Vector3(0, 0, 0),
+  scene
+);
