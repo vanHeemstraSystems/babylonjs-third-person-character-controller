b68bc80e419b29e663cab973c5ac11e22efa5f24
Willem van Heemstra <wvanheemstra@icloud.com>
2024-01-03 16:15:53 +0000

    Update thirdPersonCharacterController

--- containers/app/babylonjs/src/scenes/thirdPersonCharacterController.ts
+++ containers/app/babylonjs/src/scenes/thirdPersonCharacterController.ts
@@ -1,1 +1,57 @@
-// TO DO
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder";
+import { ExecuteCodeAction } from "@babylonjs/core/Actions/directActions";
+import { ActionManager } from "@babylonjs/core/Actions/actionManager";
+
+// import '@babylonjs/loaders/glTF/2.0/index.js';
+
+const canvas = document.getElementById('renderCanvas');
+const engine = new Engine(canvas, true);
+const scene = new Scene(engine);
+
+new HemisphericLight('hemiLight', new Vector3(0, 1, 0));
+
+// Camera
+const camera = new ArcRotateCamera(
+  'arcRotateCamera',
+  0,
+  1,
+  10,
+  new Vector3(0, 0, 0),
+  scene
+);
+camera.speed = 0.1;
+camera.attachControl(canvas, true);
+
+// Render
+engine.runRenderLoop(() => {
+  scene.render();
+});
+
+// Resize
+window.addEventListener('resize', () => {
+  engine.resize();
+});
+
+scene.createDefaultEnvironment({
+  createGround: false,
+  createSkybox: false
+});
+CreateGround('ground', { width: 50, height: 50 });
+
+camera.wheelPrecision = 10;
+
+// Model
+const loadModel = async () => {
+  const model = await SceneLoader.ImportMeshAsync('null', 'https://assets.babylonjs.com/meshes/', 'HVGirl.glb', scene);
+  const player = model.meshes[0];
+}
+
+loadModel();
+
+// MORE
