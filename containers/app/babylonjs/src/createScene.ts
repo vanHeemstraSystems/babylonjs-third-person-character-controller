b74baa9fcd41a632831b29af76d41fe467fa4951
Willem van Heemstra <wvanheemstra@icloud.com>
2024-01-09 09:48:14 +0000

    Working: find fix for importing external scenes

--- containers/app/babylonjs/src/createScene.ts
+++ containers/app/babylonjs/src/createScene.ts
@@ -3,8 +3,8 @@ import type { Scene } from "@babylonjs/core/scene";
 
 // Change this import to check other scenes
 import { DefaultSceneWithTexture } from "./scenes/defaultWithTexture"; // Locally
-// import { ThirdPersonCharacterController } from "./scenes/thirdPersonCharacterController"; // Locally
-import { ThirdPersonCharacterController } from "https://github.com/vanHeemstraSystems/babylonjs-scene-third-person-character-controller/raw/main/scene.ts"; // Remotelly
+import { ThirdPersonCharacterController } from "./scenes/thirdPersonCharacterController"; // Locally
+// import { ThirdPersonCharacterController } from "https://github.com/vanHeemstraSystems/babylonjs-scene-third-person-character-controller/raw/main/scene.ts"; // Remotelly, currently not working due to error: "The target environment doesn't support dynamic import() syntax so it's not possible to use external type 'module' within a script"
 
 export interface CreateSceneClass {
     createScene: (engine: Engine, canvas: HTMLCanvasElement) => Promise<Scene>;
